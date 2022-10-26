cd8e7bf6a875e92246354c87d0f9900bb8bd223a
Erika <erikapalmasan@gmail.com>
2022-10-26 17:58:13 +0200

    first commit

---
+++ src/main/java/com/nttdatabootcamp/saludostarter/SaludoStarterApplication.java
@@ -0,0 +1,13 @@
+package com.nttdatabootcamp.saludostarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludoStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludoStarterApplication.class, args);
+	}
+
+}
